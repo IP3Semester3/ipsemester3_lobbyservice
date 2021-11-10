a366609ca1f304c2508f8499a1e92d1901a5d7e2
Eykholt,Tom T.T.H <t.eykholt@student.fontys.nl>
2021-11-10 09:57:48 +0100

    Update

--- src/main/java/lobbyservice/lobbyservice/LobbyserviceApplication.java
+++ src/main/java/lobbyservice/lobbyservice/LobbyserviceApplication.java
@@ -3,6 +3,9 @@ package lobbyservice.lobbyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+    Runs the application
+ */
 @SpringBootApplication
 public class LobbyserviceApplication {
 	public static void main(String[] args) {
